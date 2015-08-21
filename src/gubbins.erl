a184036a462170da9a3549e3851504530103bb39
Gordon Guthrie <gguthrie@basho.com>
2015-08-21 16:59:09 +0000

    more playing about

--- src/gubbins.erl
+++ src/gubbins.erl
@@ -15,26 +15,61 @@
 
 testing() ->
     io:format("in test~n"),
-    DDL = get_ddl(),
-    Map = create_map(DDL),
+    {PDDL,     PMod}  = get_primary_ddl(),
+    {_SDDLMod, _SMod} = get_secondary_ddl(),
+    Map = create_map(PMod, PDDL, {"user1", "gordon", "guthrie", 52, "holiday account"}),
     io:format("Map is ~p~n", [Map]),
     ok.
 
-create_map(#ddl_v1{fields    = F,
-		   local_key = LK}) ->
-    io:format(user, "Fields is ~p~nLK is ~p~n", [F, LK]),
-    erko.
+create_map(Mod, #ddl_v1{fields    = F,
+			local_key = LK}, Row) ->
+    F2 = strip_fields(F, LK),
+    io:format("Row is ~p~n", [Row]),
+    io:format("F2 is ~p~n", [F2]),
+    Map = riak_dt_map:new(),
+    AddFieldFun = fun(K, Mp) ->
+			  io:format("K is ~p~n", [K]),
+			  Val = Mod:extract(Row, K),
+			  io:format("Val is ~p~n", [Val]),
+			  Op = bip,
+			  Actor = yerp,
+			  riak_dt_map:update(Op, Actor, Mp)
+		  end,
+    _NewMap = lists:foldl(AddFieldFun, Map, F2).
 
-get_ddl() ->
+strip_fields(Fields, #key_v1{ast = Keys}) ->
+    Exclusions = [Nm || #param_v1{name = Nm} <- Keys],
+    StripFun = fun(#riak_field_v1{name = Nm}, Acc) ->
+		       case lists:member(Nm, Exclusions) of
+			   true  -> Acc;
+			   false -> [[Nm] | Acc]
+		       end
+	       end,
+    lists:foldl(StripFun, [], Fields).			       
+
+get_primary_ddl() ->
     Statement = "CREATE TABLE keytable "
 	++ "(userid varchar not null, "
 	++ "firstname varchar not null, "
 	++ "lastname varchar not null, "
 	++ "age int, "
+	++ "account_name varchar not null, "
 	++ "primary key(userid))",
+    get_ddl(Statement).
+
+get_secondary_ddl() ->
+    Statement = "CREATE TABLE details "
+	++ "(userid varchar not null, "
+	++ "auto varchar not null, "
+	++ "date int not null, "
+	++ "amount float not null, "
+	++ "primary key(userid, auto))",
+    get_ddl(Statement).
+    
+get_ddl(Statement) ->
     Toks = riak_ql_lexer:get_tokens(Statement),
-    io:format(user, "Toks is ~p~n", [Toks]),
     {ok, DDL} = riak_ql_parser:parse(Toks),
-    DDL.
+    {module, Mod} = riak_ql_ddl_compiler:make_helper_mod(DDL, "/tmp"),
+    {DDL, Mod}.
 
 
